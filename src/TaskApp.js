aaaf035dd6241acfb38b0e2b531ee03875869503
Nicolas Carignani <2bccarignani3@gmail.com>
2021-04-27 23:37:50 -0300

    state refactoring

--- src/TaskApp.js
+++ src/TaskApp.js
@@ -4,61 +4,70 @@ import TaskContent from './TaskContent';
 import TaskListPicker from './TaskListPicker';
 
 function TaskApp() {
-    const [tasks, setTasks] = useState([
+    const [lists, setLists] = useState([
+        { id: 1, name : 'TODO' }, { id: 2, name : 'Pending'}, { id: 3, name: 'Prueba'}
+    ]);
+
+    const [nonCurrentTasks, setNonCurrentTasks] = useState([
         {
-            id: 1,
-            list : 'TODO',
+            listId: 2,
             tasks: [
-                {id: 1, title: 'First task', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque erat massa, aliquet eu lobortis nec, efficitur sit amet ipsum. Phasellus at quam sodales, rhoncus velit vitae, venenatis mauris. Sed pharetra.'},
-                {id: 2, title: 'Second task', description: 'Nullam nec commodo quam. Maecenas eget feugiat odio. Morbi feugiat eleifend dictum. Quisque egestas ligula vitae tellus sagittis convallis.'},
-                {id: 3, title: 'Third task', description: 'Aenean sodales neque lorem, vel maximus felis lobortis a. Duis lobortis mauris leo, sed pharetra augue condimentum nec. Praesent dictum leo vel sem consectetur, eget blandit nisl blandit.'},
+                {id: 1, title: 'PENDING task', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque erat massa, aliquet eu lobortis nec, efficitur sit amet ipsum. Phasellus at quam sodales, rhoncus velit vitae, venenatis mauris. Sed pharetra.'},
+                {id: 2, title: 'PENDING task', description: 'Nullam nec commodo quam. Maecenas eget feugiat odio. Morbi feugiat eleifend dictum. Quisque egestas ligula vitae tellus sagittis convallis.'},
+                {id: 3, title: 'PENDING task', description: 'Aenean sodales neque lorem, vel maximus felis lobortis a. Duis lobortis mauris leo, sed pharetra augue condimentum nec. Praesent dictum leo vel sem consectetur, eget blandit nisl blandit.'},
             ]
         },
         {
-            id: 2,
-            list : 'Pending',
+            listId: 3,
             tasks: [
-                {id: 1, title: 'First pending task', description: 'Task description'},
-                {id: 2, title: 'Second pending task', description: 'Second description'},
-                {id: 3, title: 'Third pending task', description: 'Third description'},
+                {id: 1, title: 'Prueba task', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque erat massa, aliquet eu lobortis nec, efficitur sit amet ipsum. Phasellus at quam sodales, rhoncus velit vitae, venenatis mauris. Sed pharetra.'},
             ]
-        }, 
-    ]);
-    const [currentTaskIndex, setCurrentTask] = useState(0);
-    const [currentListIndex, setCurrentList] = useState(0);
+        }
+    ])
+    const [currentTasks, setCurrentTasks] = useState([
+            {id: 1, title: 'TODO task', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque erat massa, aliquet eu lobortis nec, efficitur sit amet ipsum. Phasellus at quam sodales, rhoncus velit vitae, venenatis mauris. Sed pharetra.'},
+            {id: 2, title: 'TODO task', description: 'Nullam nec commodo quam. Maecenas eget feugiat odio. Morbi feugiat eleifend dictum. Quisque egestas ligula vitae tellus sagittis convallis.'},
+            {id: 3, title: 'TODO task', description: 'Aenean sodales neque lorem, vel maximus felis lobortis a. Duis lobortis mauris leo, sed pharetra augue condimentum nec. Praesent dictum leo vel sem consectetur, eget blandit nisl blandit.'},
+    ])
+    const [currentTaskIndex, setCurrentTaskIndex] = useState(0);
+    const [currentListIndex, setCurrentListIndex] = useState(0);
 
-    const taskLists = tasks.map(t => { return {id: t.id, name: t.list} });
-    const currentList = tasks[currentListIndex];
+    const currentTask = currentTasks[currentTaskIndex];
+    const currentList = lists[currentListIndex];
+
 
     const newTask = () => {
-        const newT = {id: currentList.tasks.length + 1, title: 'New task', description: 'New task description.'};
-        const l = currentList.tasks;
-        const newTs = [newT, ...l];
-        const tasksAux = [...tasks];
-        tasksAux[currentListIndex].tasks = newTs;
-        setTasks(tasksAux);
-        setCurrentTask(0);
+        const newT = {id: currentTasks.length + 1, title: 'New task', description: 'New task description.'};
+        const tasks = [newT, ...currentTasks];
+        setCurrentTasks(tasks);
+        setCurrentTaskIndex(0);
     }
 
     const changeList = (id) => {
         if(currentList.id !== id){
-            setCurrentList(tasks.findIndex(l => l.id === id));
-            setCurrentTask(0);
+            let ncurrTasks = nonCurrentTasks.filter(({listId}) => listId !== id);
+            ncurrTasks.push({listId: currentList.id, tasks: currentTasks});
+            const newCurrentTasks = nonCurrentTasks.find(({listId}) => listId === id).tasks;
+
+            setNonCurrentTasks(ncurrTasks);
+            setCurrentTasks(newCurrentTasks);
+            setCurrentListIndex(lists.findIndex(l => l.id === id));
+            setCurrentTaskIndex(0);
         }
     }
 
     const updateCurrentTask = ({title, description}) => {
-        const updT = Object.assign({...currentList.tasks[currentTaskIndex]}, {title, description});
-        const tasksAux = [...tasks];
-        tasksAux[currentListIndex].tasks[currentTaskIndex] = updT;
-        setTasks(tasksAux);
+        const updT = Object.assign({...currentTask}, {title, description});
+        const currTasks = [...currentTasks];
+        currTasks[currentTaskIndex] = updT;
+        setCurrentTasks(currTasks);
     }
 
     return (
         <div className="Task-App">
             <div className="task-app-header">
                 <TaskListPicker 
-                    lists={taskLists} 
+                    lists={lists} 
                     current={currentList.id} 
                     onChange={changeList}
                 /> 
@@ -66,15 +75,15 @@ function TaskApp() {
             <div className="task-app-content">
                 <div className="task-list">
                     <TaskList 
-                        tasks={currentList.tasks}
-                        current={currentList.tasks[currentTaskIndex].id} 
-                        onChange={(id) => setCurrentTask(currentList.tasks.findIndex(t => t.id === id))}
+                        tasks={currentTasks}
+                        current={currentTask.id} 
+                        onChange={(id) => setCurrentTaskIndex(currentTasks.findIndex(t => t.id === id))}
                         onNewTask={newTask}
                     />
                 </div>
                 <div className="task-content">
                     <TaskContent 
-                        task={currentList.tasks[currentTaskIndex]} 
+                        task={currentTask} 
                         onChange={(v) => updateCurrentTask(v)}
                     />
                 </div>
